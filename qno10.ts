9db96bc318afdb8f04ac9e9d98622c31ae005cdb
mskshafi <139792967+mskshafi@users.noreply.github.com>
2023-07-30 16:34:54 +0500

    Add files via upload

---
+++ qno10.ts
@@ -0,0 +1,8 @@
+// This program prints the sum of two numbers
+let num1 = 5;
+let num2 = 3;
+console.log("My Name is Muhammad Shahzad Khan, batch 47, today date is 30-July-2023")
+console.log(num1 + num2); // Output: 8
+
+// This program prints a message to the console
+console.log("Hello, world!"); // Output: Hello, world!
