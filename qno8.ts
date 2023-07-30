00c261e9ac5f64683a9306e03a4b512dea322858
mskshafi <139792967+mskshafi@users.noreply.github.com>
2023-07-30 16:12:40 +0500

    Add files via upload

---
+++ qno8.ts
@@ -0,0 +1,11 @@
+var a=(5+3);
+var b=(10-2);
+var c=(2*4);
+var d=(16/2);
+//var Z = "Print "
+console.log ("______________________________")
+console.log (a);
+console.log (b);
+console.log (c);
+console.log (d);
+console.log ("______________________________")
