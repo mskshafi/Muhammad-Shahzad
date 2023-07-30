75a8cbc6a4b917b6ca589c1a09694e72d6236b8d
mskshafi <139792967+mskshafi@users.noreply.github.com>
2023-07-30 13:39:41 +0500

    Add files via upload

---
+++ qno3.ts
@@ -0,0 +1,11 @@
+// Declare a variable to store the person's name
+let Name = "shahzad";
+
+// Print the name in lowercase
+console.log(Name.toLowerCase());
+
+// Print the name in uppercase
+console.log(Name.toUpperCase());
+
+// Print the name in titlecase
+console.log(Name[0].toUpperCase() + Name.slice(1).toLowerCase());
