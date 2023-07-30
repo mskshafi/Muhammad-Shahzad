1fd7a28beb792462cfe48aa6a165df00a56202bf
mskshafi <139792967+mskshafi@users.noreply.github.com>
2023-07-30 14:34:44 +0500

    Add files via upload

---
+++ qno5.ts
@@ -0,0 +1,5 @@
+var famous_person = "Albert Einstein! "
+var Message = "Life is like riding a bicycle. To keep your balance, you must keep moving."
+//var Print = famous_person + Message;
+//console.log (Print)
+console.log (famous_person+Message);
