982da946874d4082358cc940873e5e8b4793837a
mskshafi <139792967+mskshafi@users.noreply.github.com>
2023-07-30 15:19:18 +0500

    Add files via upload

---
+++ qno6.ts
@@ -0,0 +1,9 @@
+var out = "hello world".replace(/\s/g, "");
+//\t for tab
+var a = "\t Hello \tLahore City"
+var b = "\n Hello Islamabad Capital"
+var c = "\t Hello \tLahore City".replace(/\s/g, "");
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(out);
