a89caf5896cfbbfd4ffd811a5a3b6991ae1e81cd
mskshafi <139792967+mskshafi@users.noreply.github.com>
2023-08-06 22:34:07 +0500

    Add files via upload

---
+++ QNO03.ts
@@ -0,0 +1,5 @@
+let name1= "Muhammad Shahzad Khan"
+console.log(name1.toLocaleLowerCase());
+console.log(name1.toLocaleUpperCase());
+console.log(name1.toLowerCase());
+console.log(name1.toUpperCase());
